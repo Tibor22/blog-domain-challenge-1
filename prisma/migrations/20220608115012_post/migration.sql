dbf7317603df5161bbf1bb06a929eca5dcd8f8f1
Tibor22 <tothtibitravel@gmail.com>
2022-06-08 14:36:55 +0100

    all requirements finished with createMany

---
+++ prisma/migrations/20220608115012_post/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "ImgUrl" TEXT,
+    "title" VARCHAR(150) NOT NULL,
+    "contect" VARCHAR(500) NOT NULL,
+    "isPublished" BOOLEAN NOT NULL DEFAULT false,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/seed.js
+++ prisma/seed.js
@@ -2,19 +2,73 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function seed() {
+
     const createdUsers = await prisma.user.createMany({
         data: [
-            { username: 'alicemartin' },
-            { username: 'alicemartin' }
+            {
+                 username: 'alicemar1',
+                 email: 'alicemartin1@gmail.com',
+            },
+            { 
+                username: 'alicemar2',
+                email: 'alicemartin2@gmail.com',
+            }
         ]
     });
+
+    const createProfiles = await prisma.profile.createMany({
+        data: [
+            {
+                       profileImgUrl: 'https://imagerandom.com',
+                       bio:'some random things about me',
+                       userId: 1,
+            },
+            {
+                       profileImgUrl: 'https://imagerandom2.com',
+                       bio:'some random things about me as well',
+                       userId:2,
+                    }
+           
+
+        ]
+    })
+
+    const createPosts = await prisma.post.createMany({
+        data: [
+            {
+               title:'200 years before',
+               content:'Long long time ago',
+               isPublished: true,
+               userId: 1,
+            },
+            {
+               title:'400 years before',
+               content:'Long long LOOOONG time ago',
+               isPublished: true,
+               userId: 2,
+               ImgUrl:'https://imagerandom.com'
+            },
+        ]
+    })
+
+  const createComments = await prisma.comment.createMany({
+    data: [
+            {
+                content:'Nice one mate',
+                userId: 1,
+                postId:2,
+             },
+             {
+                content:'Long time no see',
+                userId: 2,
+                postId:2,
+             },
+    ]
+  })
 
     console.log(`${createdUsers.count} users created`, createdUsers);
 
     // Add your code here
-
-    
-
 
     // Don't edit any of the code below this line
     process.exit(0);
